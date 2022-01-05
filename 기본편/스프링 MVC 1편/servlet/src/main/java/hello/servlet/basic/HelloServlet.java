b32e604472cc3ae25e933c7c035d78e29a6426bb
이경창 <lkc263@naver.com>
2022-01-05 18:59:39 +0900

    ✏️ Study Servlet, to next Time

---
+++ 기본편/스프링 MVC 1편/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,21 @@
+package hello.servlet.basic;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// hello로 오면 이게 실행된다.
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+    }
+}
