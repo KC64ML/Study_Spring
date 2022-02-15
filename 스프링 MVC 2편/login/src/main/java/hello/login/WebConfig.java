f24fe825212fb8f269fbb7544ed2fe8e0d407a88
이경창 <lkc263@naver.com>
2022-02-15 19:35:19 +0900

    📌 로그인 동작 원리 이해하는 중

--- 스프링 MVC 2편/login/src/main/java/hello/login/WebConfig.java
+++ 스프링 MVC 2편/login/src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,6 @@
 package hello.login;
 
-import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
+//import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
@@ -18,10 +18,10 @@ import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new LoginMemberArgumentResolver());
-    }
+//    @Override
+//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+//        resolvers.add(new LoginMemberArgumentResolver());
+//    }
 
     //    @Bean
     public FilterRegistrationBean logFilter() {
