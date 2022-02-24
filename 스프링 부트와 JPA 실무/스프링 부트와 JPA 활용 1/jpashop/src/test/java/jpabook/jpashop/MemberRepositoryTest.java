07c67a444ceaefdb6ce0cb5e6ea0cc8107a1135a
이경창 <lkc263@naver.com>
2022-02-24 18:48:47 +0900

    🔊 jpa 시작, 엔터티 클래스 개발까지 완료!

--- 스프링 부트와 JPA 실무/스프링 부트와 JPA 활용 1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ 스프링 부트와 JPA 실무/스프링 부트와 JPA 활용 1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,12 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.awt.*;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,6 +18,7 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(false)
     public void testMember() throws Exception{
         // given
         Member member = new Member();
@@ -32,6 +31,10 @@ public class MemberRepositoryTest {
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        System.out.println("findMember == member: " + (findMember = member));
+
     }
 
 
