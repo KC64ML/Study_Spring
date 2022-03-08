0812d024aec18064cd132312531616365cab021b
이경창 <lkc263@naver.com>
2022-03-08 18:15:52 +0900

    🔊 연관관계 매핑 기초 - 매핑시작, 다대일[N:1]

--- 스프링 부트와 JPA 실무/자바 ORM 표준 JPA 프로그래밍 - 기본편/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 스프링 부트와 JPA 실무/자바 ORM 표준 JPA 프로그래밍 - 기본편/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -11,10 +11,29 @@ public class OrderItem {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @Column(name = "ITEM_ID")
-    private Long itemId;
-    @Column(name = "ORDER_ID")
-    private Long orderId;
+    @ManyToOne
+    @JoinColumn(name="ORDER_ID")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name ="ITEM_ID")
+    private Item item;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
 
     private int orderPrice; //주문 가격
     private int count;      //주문 수량
@@ -27,21 +46,6 @@ public class OrderItem {
         this.id = id;
     }
 
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public Long getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(Long itemId) {
-        this.itemId = itemId;
-    }
 
     public int getOrderPrice() {
         return orderPrice;
