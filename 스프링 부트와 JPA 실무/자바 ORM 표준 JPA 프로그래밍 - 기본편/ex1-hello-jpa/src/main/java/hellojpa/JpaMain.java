276df04886a70f3845808e976d3f62e830b9ac9c
이경창 <lkc263@naver.com>
2022-03-15 18:44:46 +0900

    ✏️ 객체지향 쿼리 언어(JPQL) - 객체지향 쿼리 언어, 기본문법과 쿼리 API, 프로젝션, 페이징

--- 스프링 부트와 JPA 실무/자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 스프링 부트와 JPA 실무/자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,36 +17,6 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", " 10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1","street", "5000"));
-            member.getAddressHistory().add(new AddressEntity("old2","street", "7000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("======== START ===========");
-            Member findMember = em.find(Member.class, member.getId());
-
-//            // homeCity → newCity
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-//
-//            // 치킨 → 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("우유");
-
-            // 주소 변경
-//            findMember.getAddressHistory().remove(new Address("old1","street", "5000"));
-//            findMember.getAddressHistory().add(new Address("newCity1","street", "5000"));
 
             tx.commit();
         }catch (Exception e){
