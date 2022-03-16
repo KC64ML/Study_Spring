8963d8a16e8b3e6756df4c18cf4c1e0a7a36ac27
이경창 <lkc263@naver.com>
2022-03-16 17:48:54 +0900

    ✏️ 객체지향 쿼리 언어1 - 기본 문법, 중급 문법(진행중)

--- 스프링 부트와 JPA 실무/자바 ORM 표준 JPA 프로그래밍 - 기본편/jpql/src/main/java/jpql/JpaMain.java
+++ 스프링 부트와 JPA 실무/자바 ORM 표준 JPA 프로그래밍 - 기본편/jpql/src/main/java/jpql/JpaMain.java
@@ -14,26 +14,41 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member = new Member();
-            member.setUsername("teamA");
-            member.setAge(10);
-
-            member.setTeam(team);
-
+            member.setUsername("회원1");
+            member.setTeam(teamA);
             em.persist(member);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m from Member m left join Team t on m.username = t.name";
+            String query = "select m from Member m join fetch m.team";
+
             List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
-            System.out.println("result = " + result.size());
+
+            for (Member s : result) {
+                System.out.println("member = " + s.getUsername() + ", " + s.getTeam().getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
