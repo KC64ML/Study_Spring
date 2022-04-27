72876c2e4dd0424e9bb16485810504281668f83a
이경창 <lkc263@naver.com>
2022-04-27 19:39:12 +0900

    ✏️ 스프링 데이터 JPA 확장 기능 완료!

--- 스프링 부트와 JPA 실무/스프링 데이터 JPA/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ 스프링 부트와 JPA 실무/스프링 데이터 JPA/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,9 +1,11 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -18,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @Rollback(value = false)
 class MemberTest {
 
+    @Autowired MemberRepository memberRepository;
     @PersistenceContext
     EntityManager em;
 
@@ -50,5 +53,27 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("member.getTeam() = " + member.getTeam());
         }
+    }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        // given
+        Member member = new Member("member1");
+        memberRepository.save(member); // @PrePersist
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush(); // @PreUpdate
+        em.clear();
+
+        // when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        // then
+        System.out.println("findMember.createDate = " + findMember.getCreateDate());
+        System.out.println("findMember.updateDate = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.createBy = " + findMember.getCreatedBy());
+        System.out.println("findMember.updateBy = " + findMember.getLastModifiedBy());
     }
 }
