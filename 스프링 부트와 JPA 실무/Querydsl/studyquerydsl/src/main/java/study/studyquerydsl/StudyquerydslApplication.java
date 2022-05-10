0e025584256a3f2b8f304fa933f2208dcb12d0ce
이경창 <lkc263@naver.com>
2022-05-10 19:10:50 +0900

    ✏️ 복습 진행 중

---
+++ 스프링 부트와 JPA 실무/Querydsl/studyquerydsl/src/main/java/study/studyquerydsl/StudyquerydslApplication.java
@@ -0,0 +1,13 @@
+package study.studyquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyquerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyquerydslApplication.class, args);
+	}
+
+}
